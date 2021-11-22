a1abf710e830d46590e3768ca5f3fbb8c0ea2b1d
neutralboy <leopoorna99@gmail.com>
2021-11-22 11:51:54 +0530

    changed image order

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -130,12 +130,12 @@
         "https://res.cloudinary.com/poorna/image/upload/v1637334339/tss/mens-wear/Screenshot_from_2021-11-19_20-34-58.png"
     ]
     const LIGHTBOX_PICTURS_womensWear = [
-        "https://res.cloudinary.com/poorna/image/upload/v1637411139/tss/womens-wear/WhatsApp_Image_2021-11-12_at_17.12.41.jpg",
-        "https://res.cloudinary.com/poorna/image/upload/v1637411108/tss/womens-wear/WhatsApp_Image_2021-11-12_at_17.12.19.jpg",
         "https://res.cloudinary.com/poorna/image/upload/v1637334414/tss/womens-wear/WhatsApp_Image_2021-11-12_at_17.14.36.jpg",
         "https://res.cloudinary.com/poorna/image/upload/v1637557392/tss/womens-wear/image4.jpg",
         "https://res.cloudinary.com/poorna/image/upload/v1637557393/tss/womens-wear/image3.jpg",
         "https://res.cloudinary.com/poorna/image/upload/v1637557394/tss/womens-wear/image2.jpg"
+        "https://res.cloudinary.com/poorna/image/upload/v1637411139/tss/womens-wear/WhatsApp_Image_2021-11-12_at_17.12.41.jpg",
+        "https://res.cloudinary.com/poorna/image/upload/v1637411108/tss/womens-wear/WhatsApp_Image_2021-11-12_at_17.12.19.jpg"
     ]
     const LIGHTBOX_PICTURS_outerWear = [
         "https://res.cloudinary.com/poorna/image/upload/v1637334561/tss/outer-wear/Screenshot_from_2021-11-19_20-38-39.png",
